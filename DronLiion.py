ef9ef0db128c1185661201933720b6c46f12e1b5
DavidGom1 <gomsk8jumi@gmail.com>
2024-12-18 13:43:48 +0100

    feat(back): :sparkles: cambio de archivos csv a xml - refactorizacion de algunas variables
    Se ha cambiado la extension de los archivos para que use XML en lugar de CSV, además se han refactorizado buena cantidad de variables en el codigo

--- DronLiion.py
+++ DronLiion.py
@@ -1,14 +1,14 @@
-import csv
+import xml.etree.ElementTree as ET
 
-def CargarCeldasCSV(file_path): # Carga de datos de las celdas desde el CSV
+def CargarCeldasXML(file_path): # Carga de datos de las celdas desde el archivo XML
     celdas = []
-    with open(file_path, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            for datos_celdas in row:
-                if ':' in datos_celdas:
-                    capacidad, resistencia = map(int, datos_celdas.split(':'))
-                    celdas.append({'capacidad': capacidad, 'resistencia': resistencia})
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    for celda in root.findall('celda'):
+        id_celda = celda.get('id')
+        capacidad = int(celda.find('capacidad').text)
+        resistencia = int(celda.find('resistencia').text)
+        celdas.append({'id': id_celda, 'capacidad': capacidad, 'resistencia': resistencia})
     return celdas
 
 def OrdenCeldasPorCriterio(celdas, priorizar_capacidad=True): # Ordenar las celdas por capacidad o resistencia
@@ -26,15 +26,16 @@ def ConstruccionBateria(celdas, cantidadSerie, cantidadParalelo): # Construcció
     return celdasSeleccionadas
 
 def CalculoEspecificacionBateria(celdasSeleccionadas): # Cálculo de la capacidad total y resistencia media
-    capacidadTotal = sum(min(group, key=lambda x: x['capacidad'])['capacidad'] for group in celdasSeleccionadas)/cantidadParalelo
+    capacidadTotal = sum(min(group, key=lambda x: x['capacidad'])['capacidad'] for group in celdasSeleccionadas) / len(celdasSeleccionadas[0])
     resistenciaMedia = sum(sum(cell['resistencia'] for cell in group) / len(group) for group in celdasSeleccionadas) / len(celdasSeleccionadas)
     return capacidadTotal, resistenciaMedia
 
+file_path = "./almacen1.xml" #input("Ingrese la ruta de su archivo XML: ")
+
 if __name__ == "__main__": # Ejecución del programa
-    file_path = input("Ingrese la ruta de su archivo CSV:")
-    celdas = CargarCeldasCSV(file_path)
-    if not celdas: # Verificación de la existencia de celdas en el archivo CSV
-        print("No se encontraron celdas en el archivo CSV.")
+    celdas = CargarCeldasXML(file_path)
+    if not celdas: # Verificación de la existencia de celdas en el archivo XML
+        print("No se encontraron celdas en el archivo XML.")
         exit()
     cantidadSerie = int(input("Ingresa el número de celdas en serie: "))
     cantidadParalelo = int(input("Ingresa el número de celdas en paralelo: "))
@@ -46,7 +47,10 @@ if __name__ == "__main__": # Ejecución del programa
     else:
         capacidadTotal, resistenciaMedia = CalculoEspecificacionBateria(celdasSeleccionadas)
         print(f"\nConfiguracion de la batería:")
+        print(f"\nGrupo en serie:")
         for idx, group in enumerate(celdasSeleccionadas, 1):
-            print(f" Grupo en serie {idx}: {[f'{cell['capacidad']}mAh/{cell['resistencia']}m\u03a9' for cell in group]}")
+            print(f"\tGrupo en paralelo {idx}:")
+            for cell in group:
+                print(f"\t\tID: {cell['id']}\tCap: {cell['capacidad']}mAh\tRes: {cell['resistencia']}Ω")
         print(f"\nCapacidad total: {capacidadTotal}mAh")
         print(f"Resistencia interna media: {resistenciaMedia:.2f}m\u03a9")
